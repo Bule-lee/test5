0aee81418bf27da4ee51aacd5d14f9be6a99183e
lizhihao <779082603@qq.com>
2020-04-02 21:55:22 +0800

    test8 branch

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -15,7 +15,7 @@ public class PaymentMain8001 {
 
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
-        System.out.println("branch-test2");
+
     }
 
 }
